0c17d8d6fc39fe930455954578cd592f1e23d08c
developersatyendra <33686367+developersatyendra@users.noreply.github.com>
2018-09-18 03:20:39 -0700

    Update claim.php

--- chatroom/claim.php
+++ chatroom/claim.php
@@ -1,42 +1,43 @@
 <?php
+
 $room = $_POST['room'];
 
    include 'db_connect.php';
 
-// Check if room is already exist
+// connection check
 
-$sql = "SELECT * FROM `rooms` WHERE roomname = '$room'";
-$result = mysqli_query($conn, $sql);
-if($result)
-{
-	if (mysqli_num_rows($result) > 0) 
-	{
-		$message = "Please choose the different room name this one is already claimed";
-		echo '<script language="javascript">';
-		echo 'alert("'.$message.'");';
-		echo 'window.location="http://localhost/chatroom";';
-		echo '</script>';
-	}
+	if(!$conn)
+		{
+			die("Failed to connection". mysqli_connect_error());
+		}
 
 	else
-	{
-		$sql = "INSERT INTO `rooms` (`roomname`, `stime`) VALUES ('$room', CURRENT_TIMESTAMP);";
-		if (mysqli_query($conn, $sql)) 
 		{
-			$message = "Your room is ready for chat";
-			echo '<script language="javascript">';
-			echo 'alert("'.$message.'");';
-			echo 'window.location="http://localhost/chatroom/rooms.php?roomname="{$room}"';
-			echo '</script>';
 
+ 			$sql = "SELECT * FROM `rooms` WHERE roomname = '$room'";
+ 			$result = mysqli_query($conn, $sql);
+
+			if($result)
+               {
+			       if (mysqli_num_rows($result) > 0) 
+		           {
+						echo "Try some different room name";
+		  			}
+        
+    				else
+						{
+							$sql = "INSERT INTO ROOMS (roomname, stime) VALUES ('$room', CURRENT_TIMESTAMP);";
+							if (mysqli_query($conn, $sql))
+							{
+		
+							$message = "Your room is ready for chat";
+							echo '<script language="javascript">';
+							echo 'alert("'.$message.'");';
+							echo 'window.location="http://localhost/chatroom/rooms.php?roomname='. $room.'";';
+							echo '</script>';
+							}
+						}	
+				}	
 		}
-	}	
-}
-
-    else 
-
-      {
-		echo "mysql_error($conn)";
-	  }	
 
 ?>
